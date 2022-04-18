971298baf94ea78c05d8263e4088883407c3f287
JustAmalll <amalkrasavchik@gmail.com>
2022-04-18 17:30:13 +0600

    CompanyListingsViewModel setup

---
+++ app/src/main/java/dev/amal/stockmarketapp/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package dev.amal.stockmarketapp.presentation.company_listings
+
+import dev.amal.stockmarketapp.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
